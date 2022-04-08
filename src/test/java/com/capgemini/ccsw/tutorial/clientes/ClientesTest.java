f8c36ed958a2c18032111c2e454136afaf3e1edc
Al Youssfi Martín external <javier.al-youssfi-martin-external@capgemini.com>
2022-04-08 09:14:34 +0200

    proyecto agregado

---
+++ src/test/java/com/capgemini/ccsw/tutorial/clientes/ClientesTest.java
@@ -0,0 +1,92 @@
+package com.capgemini.ccsw.tutorial.clientes;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import com.capgemini.ccsw.tutorial.clientes.model.ClientesDto;
+
+/**
+* @author ccsw
+*/
+@SpringBootTest
+@Transactional
+public class ClientesTest
+{
+	@Autowired
+	private ClientesController c_c;
+	@Test
+	public void devolver_todas_los_clientes()
+	{
+		assertNotNull(c_c);
+		int clientest=5;
+		
+		List<ClientesDto> clientes=c_c.findAll();
+		
+		
+		
+		assertNotNull(clientes);
+		assertEquals(clientest,clientes.size());
+		
+		
+	}
+	/*
+	@Test
+	public void guardar_sin_id_deberia_crear_una_nuevo_cliente()
+	{
+		assertNotNull(c_c);
+		String nuevo_Cliente_nombre="Juan";
+		long id_nuevo=4;
+		
+		ClientesDto dto=new ClientesDto();
+		dto.setNombre(nuevo_Cliente_nombre);
+		
+		c_c.save(null, dto);
+		
+		List<ClientesDto>clientes=c_c.findAll();
+		
+		
+		assertNotNull(clientes);
+		assertEquals(id_nuevo,clientes.size());
+		
+		ClientesDto busqueda_clientes=clientes.stream().filter(item->item.getId().equals(id_nuevo)).findFirst().orElse(null);
+		assertNotNull(busqueda_clientes);
+		assertEquals(nuevo_Cliente_nombre,busqueda_clientes.getNombre());
+	}
+	*/
+	@Test
+	public void si_existe_el_mismo_nombre_no_se_guarda()
+	{
+		assertNotNull(c_c);
+		
+		String nuevo="Xavi";
+		long id=1;
+		ClientesDto dto=new ClientesDto();
+		dto.setNombre(nuevo);
+		
+		
+		c_c.save(null, dto);
+		List<ClientesDto>clientes=c_c.findAll();
+		
+		
+	    
+	    
+		
+		
+		
+		
+		
+	
+		assertNotNull(clientes);
+		
+		//assertEquals(id,clientes.size());
+	}
+	
+
+}
